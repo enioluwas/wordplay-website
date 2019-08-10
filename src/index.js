ac4d4e2f787d55454c7e12dc0ab4357c191d4c8b
Enioluwa Segun <eos24@drexel.edu>
2019-08-09 21:02:19 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-// Disable React Dev Tools
+// Disable React Dev Tools in Production
 if (process.env.NODE_ENV === 'production') {
   if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
     for (const [key, value] of Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
@@ -15,8 +15,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
