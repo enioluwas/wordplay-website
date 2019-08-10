365f04272845a23f0c7ca1ab0469d63e4995b602
Enioluwa Segun <eos24@drexel.edu>
2019-08-09 21:15:53 -0400

    Updates

---
+++ precache-manifest.c3e7a1e3175c661879bb3a518475bc9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625ae9f2809d388779660c1481062bc8",
+    "url": "/wordplay-website/index.html"
+  },
+  {
+    "revision": "3b70d41e7623863fd46c",
+    "url": "/wordplay-website/static/css/main.c2467aa4.chunk.css"
+  },
+  {
+    "revision": "c90e13d905347f8c662a",
+    "url": "/wordplay-website/static/js/2.9a97b0e4.chunk.js"
+  },
+  {
+    "revision": "3b70d41e7623863fd46c",
+    "url": "/wordplay-website/static/js/main.769f4256.chunk.js"
+  },
+  {
+    "revision": "1a90569a76a09a0ee2dc",
+    "url": "/wordplay-website/static/js/runtime~main.fa82aadd.js"
+  },
+  {
+    "revision": "65dca350b58b3be107863bc275b070ca",
+    "url": "/wordplay-website/static/media/app-logo.65dca350.svg"
+  }
+]);
